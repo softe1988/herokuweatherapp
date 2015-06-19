052a678cb7c36862c76896d6ac46ce8eaa8ff0c3
Simone Battiste-Alleyne <softe1988@gmail.com>
2015-06-18 20:33:32 -0500

    working test page

--- Gemfile
+++ Gemfile
@@ -1,5 +1,10 @@
 source 'https://rubygems.org'
+gem 'bootstrap-sass', '~>3.3.4'
+gem 'wunderground'
 
+gem 'httparty'
+
+gem 'figaro'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.1'
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,5 +3,16 @@ class WelcomeController < ApplicationController
   end
 
   def test
+   response = HTTParty.get("http://api.wunderground.com/api/2439ab09f4ef9680/geolookup/conditions/q/TX/Dallas.json")
+ 
+  @location = response['location']['city']
+  @temp_f = response['current_observation']['temp_f']
+  @temp_c = response['current_observation']['temp_c']
+  @weather_icon = response['current_observation']['icon_url']
+  @weather_words = response['current_observation']['weather']
+  @forecast_link = response['current_observation']['forecast_url']
+  @real_feel = response['current_observation']['feelslike_f']
   end
 end
+
+ 
